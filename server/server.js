0521fddaf552ce461fb201097426fdb8b433bd29
Pratiksha Sankhe <pssankhe_b20@ce.vjti.ac.in>
2022-07-14 15:06:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -35,6 +35,14 @@ const port = 4444;
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
+
 // Setting up middlewares
 app.use(cors());
 app.use(express.json());
