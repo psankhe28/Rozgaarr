c7fa5df1d6b40936abdcd3efc83a0e3701251ab4
Pratiksha Sankhe <pssankhe_b20@ce.vjti.ac.in>
2022-07-15 13:40:45 +0530

    Update apiList.js

--- client/src/lib/apiList.js
+++ client/src/lib/apiList.js
@@ -1,4 +1,4 @@
-export const server = "https://rozgaarr-portal.herokuapp.com";
+export const server = "https://rozgaarr-portal.herokuapp.com/";
 
 const apiList = {
   login: `${server}/auth/login`,
